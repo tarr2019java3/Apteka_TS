cb01317285f5d64dd3bc4f0886e36fc256ee118b
SYTEKTOMASZ <59762629+SYTEKTOMASZ@users.noreply.github.com>
2020-03-18 14:36:59 +0100

    wywolanie funkjcji w main

--- src/main/java/pl/sda/apteka/Main.java
+++ src/main/java/pl/sda/apteka/Main.java
@@ -3,22 +3,19 @@ package pl.sda.apteka;
 public class Main {
 
     public static void main(String[] args) {
-String[] nazwa = new String[2];
-    nazwa[0]="lek1";
-    nazwa[1]="lek2";
-    double[] cena = new double[2];
-    cena[0]= 4.0;
-    cena[1]=4.0;
-    double[] refundacja = new double[2];
-    refundacja[0] = 0.2;
-    refundacja[1] = 0.5;
-    Lekarstwa lekarstwa = new Lekarstwa(nazwa, cena, refundacja);
+        String[] nazwa = new String[2];
+        nazwa[0] = "lek1";
+        nazwa[1] = "lek2";
+        double[] cena = new double[2];
+        cena[0] = 4.0;
+        cena[1] = 4.0;
+        double[] refundacja = new double[2];
+        refundacja[0] = 0.2;
+        refundacja[1] = 0.5;
+        Lekarstwa lekarstwa = new Lekarstwa(nazwa, cena, refundacja);
         System.out.println(lekarstwa.maksymalnarefudnacjja());
-        System.out.println(lekarstwa.cena("lek2",true));
-lekarstwa.wyswietl();
-
-
-
-
+        System.out.println(lekarstwa.cena("lek2", true));
+        lekarstwa.wyswietl();
+        System.out.println("Koszgt całkowity zamówienia wynosi: "+lekarstwa.kosztCalkowity());
     }
 }
